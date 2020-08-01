bf444bebaeb716acf53755405f6eb7b4baf7e4f1
RazackAbubakar <66982018+RazackAbubakar@users.noreply.github.com>
2020-08-01 13:08:55 +0000

    Update python.py

--- assign_python/python.py
+++ assign_python/python.py
@@ -15,3 +15,17 @@ while not valid:
         valid = True
     except ValueError:
         print("Incorrect data format, should be YYYY-MM-DD"))
+
+s_valid  = False
+while not s_valid:
+    try:
+
+        stime = input("Enter the time project started : ")
+        time_format = "%H:%M:%S"
+        sttime = datetime.datetime.strptime(stime, time_format)
+        if re.match(r'\d{2}:\d{2}:\d{2}', str(sttime.time())):
+            if str(sttime.time()) < '24:01':
+                time1 = sttime.time()
+                s_valid = True
+    except ValueError:
+            print("Incorrect time format, should be H:M:S")
