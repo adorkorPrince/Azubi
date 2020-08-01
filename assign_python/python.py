a0a30174192656310ad5c4cc6d1ee34f8ce0f85b
RazackAbubakar <66982018+RazackAbubakar@users.noreply.github.com>
2020-08-01 12:46:29 +0000

    Update python.py

--- assign_python/python.py
+++ assign_python/python.py
@@ -8,4 +8,16 @@ print('I have seen the commit changes down there')
 import datetime
 
 import re
-import csv 
+import csv
+
+
+
+valid = False
+while not valid:
+    try:
+        sdate = input("Enter the date for the project : ") 
+        date_format = '%Y-%m-%d'
+        sdate_obj = datetime.datetime.strptime(sdate, date_format)
+        date1 = sdate_obj.date()
+        valid = True
+    except ValueError:
